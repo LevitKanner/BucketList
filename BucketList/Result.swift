6f9188b0b75ac83d4b05154073d67a8a9abdf543
Levit Osei Kanner <lkanner21@gmail.com>
2019-12-03 12:57:55 +0000

    Adds new property

--- BucketList/Result.swift
+++ BucketList/Result.swift
@@ -16,8 +16,16 @@ struct Query: Codable {
     let pages: [Int: Page]
 }
 
-struct Page: Codable {
-    let pageId: Int
+struct Page: Codable, Comparable {
+    let pageid: Int
     let title: String
+    let ns: Int
     let terms: [String: [String]]?
+    var description: String {
+        terms?["description"]?.first ?? "No further information"
+    }
+    
+    static func < (lhs: Page, rhs: Page) -> Bool {
+        lhs.title < rhs.title
+    }
 }
