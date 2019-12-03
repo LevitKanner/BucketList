4b604ba31008d811d1747fab23496690708ebdfd
Levit Osei Kanner <lkanner21@gmail.com>
2019-12-03 10:24:09 +0000

    Adds extension on MKPointAnnotation

--- BucketList/MKPointAnnotation-ObservableObject.swift
+++ BucketList/MKPointAnnotation-ObservableObject.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2019 Levit Kanner. All rights reserved.
 //
 
-import Foundation
+import MapKit
+ 
+extension MKPointAnnotation: ObservableObject{
+    public var wrappedTitle: String{
+        get{
+            self.title ?? "No Title"
+        }
+        set{
+            self.title = newValue
+        }
+    }
+    
+    public var wrappedSubtitle: String{
+        get{
+            self.subtitle ?? "No information on this location"
+        }
+        set{
+            self.subtitle = newValue
+        }
+    }
+}
