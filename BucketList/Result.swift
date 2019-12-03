4004480615afb0e949277a9401ca9f83776424b2
Levit Osei Kanner <lkanner21@gmail.com>
2019-12-03 10:23:24 +0000

    Adds data model for fetch results from wikipaedia

--- BucketList/Result.swift
+++ BucketList/Result.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageId: Int
+    let title: String
+    let terms: [String: [String]]?
+}
