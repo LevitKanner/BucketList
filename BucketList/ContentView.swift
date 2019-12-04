9898df353d1d1c0dbec1d3a928cc544b4c46dd58
Levit Osei Kanner <lkanner21@gmail.com>
2019-12-04 11:21:42 +0000

    Adds user authentication and a button to trigger authentication

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -16,63 +16,73 @@ struct ContentView: View {
     @State private var selectedPlace: MKPointAnnotation?
     @State private var showingPlaceDetails = false
     @State private var showingEditView = false
-      
+    @State private var unLocked = false
     
     var body: some View {
- 
-                    ZStack{
-                        MapView(centerCoordinate: self.$centerCoordinate, annotations: self.locations, selectedPlace: self.$selectedPlace, showingPlaceDetails: self.$showingPlaceDetails)
-                            .edgesIgnoringSafeArea(.all)
-                        
-                        //Circle in the center of the map View
-                        Circle()
-                            .fill(Color.blue)
-                            .opacity(0.3)
-                            .frame(width: 32 , height: 32)
-                        
-                        //Adds button to the right corner of the map view
-                        VStack{
+        
+            ZStack{
+                if unLocked{
+                    MapView(centerCoordinate: self.$centerCoordinate, annotations: self.locations, selectedPlace: self.$selectedPlace, showingPlaceDetails: self.$showingPlaceDetails)
+                        .edgesIgnoringSafeArea(.all)
+                    
+                    //Circle in the center of the map View
+                    Circle()
+                        .fill(Color.blue)
+                        .opacity(0.3)
+                        .frame(width: 32 , height: 32)
+                    
+                    //Adds button to the right corner of the map view
+                    VStack{
+                        Spacer()
+                        HStack{
                             Spacer()
-                            HStack{
-                                Spacer()
-                                Button(action:{
-                                    //Button action to come soon
-                                    
-                                    let newLocation = CodableMKPointAnnotation()
-                                    newLocation.title = "Provide place name"
-                                    newLocation.coordinate = self.centerCoordinate
-                                    self.locations.append(newLocation )
-                                    
-                                    self.selectedPlace = newLocation
-                                    self.showingEditView = true
-                                    
-                                }){
-                                    Image(systemName: "plus")
-                                }
-                                .padding()
-                                .background(Color.black.opacity(0.7))
-                                .foregroundColor(Color.white)
-                                .clipShape(Circle())
-                                .shadow(radius: 0.7)
-                                .padding([.trailing , .bottom])
+                            Button(action:{
+                                //Button action to come soon
+                                
+                                let newLocation = CodableMKPointAnnotation()
+                                newLocation.title = "Provide place name"
+                                newLocation.coordinate = self.centerCoordinate
+                                self.locations.append(newLocation )
+                                
+                                self.selectedPlace = newLocation
+                                self.showingEditView = true
+                                
+                            }){
+                                Image(systemName: "plus")
                             }
-                            
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                            .foregroundColor(Color.white)
+                            .clipShape(Circle())
+                            .shadow(radius: 0.7)
+                            .padding([.trailing , .bottom])
                         }
                         
                     }
-                        
-                        
-                    .alert(isPresented: $showingPlaceDetails, content: { () -> Alert in
-                        Alert(title: Text(selectedPlace?.title ?? "Unknown place"), message: Text(selectedPlace?.subtitle ?? "Missing place information"), primaryButton: .default(Text("Okay")), secondaryButton: .default(Text("edit")){
-                            self.showingEditView = true
-                            })
-                    })
-                        .sheet(isPresented: $showingEditView,onDismiss: saveData , content: {
-                            if self.selectedPlace != nil {
-                                EditingView(placeMark: self.selectedPlace!)
-                            }
-                        })
-                        .onAppear(perform: loadData)
+                }
+                else{
+                    Button(action:{
+                        self.authenticate()
+                    }){
+                        Text("Unlock Bucketlist")
+                    }
+                .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                .clipShape(Capsule())
+                }
+            }
+        .alert(isPresented: $showingPlaceDetails, content: { () -> Alert in
+            Alert(title: Text(selectedPlace?.title ?? "Unknown place"), message: Text(selectedPlace?.subtitle ?? "Missing place information"), primaryButton: .default(Text("Okay")), secondaryButton: .default(Text("edit")){
+                self.showingEditView = true
+                })
+        })
+        .sheet(isPresented: $showingEditView,onDismiss: saveData , content: {
+                if self.selectedPlace != nil {
+                    EditingView(placeMark: self.selectedPlace!)
+                }
+            })
+        .onAppear(perform: loadData)
     }
     
     //Get file directory url
@@ -107,30 +117,25 @@ struct ContentView: View {
     
     
     //Authenticate user
-    func authenticate(){
-        
+    func authenticate() {
         let context = LAContext()
         var error: NSError?
         
         if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
-            
-            let reason = "Unlock App"
+            let reason = "Unlock Bucketlist"
             
             context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { (success, error) in
-                
+                //Push work back to main thread
                 DispatchQueue.main.async {
-                    if success {
-                        //authenticated successfully
-                        
+                    if success{
+                        self.unLocked = true
                     }else{
-                        //There was a problem
+                        debugPrint(error!)
                     }
                 }
-                
             }
-        }else{
-            //no biometrics
-            print("This device doesn't support biometric authentication")
+        }else {
+            print("Device doesn't support biometric authentication")
         }
     }
     
